c7638ec1d34f83ce91243a6ab7500741fb0ba55f
Utso Sarkar <87900549+Utso-Sarkar@users.noreply.github.com>
2022-09-27 13:41:13 +0600

    Var upload

---
+++ Open_Js_var.js
@@ -0,0 +1,63 @@
+// Variable:1
+
+
+// var banana_price = 22;
+// console.log(banana_price);
+
+
+// type of var:
+
+// var Apple =12;
+// console.log( typeof Apple);
+
+
+// var name = "Utso sarkar"
+// console.log( typeof name);
+
+
+
+// var ist = true;
+// var isnot= false;
+// console.log( typeof isnot)
+
+// var promise ="I love you oishi.Aj 26 tarik .pujar akon o 4 din baki .Aj toMake Dakhlam market a .Sundor lagcilo tomake .";
+// console.log(promise.toLowerCase());
+
+
+
+var promise ="I love you oishi.Aj 26 tarik .pujar akon o 4 din baki .Aj toMake Dakhlam market a .Sundor lagcilo tomake .";
+ console.log(promise.indexOf("oishi"));
+
+
+// var promise ="I love you oishi.Aj 26 tarik .pujar akon o 4 din baki .Aj toMake Dakhlam market a .Sundor lagcilo tomake .";
+// console.log(promise.split(""));
+
+number1=10;
+number2="20.20";
+number2=parseFloat(number2)
+sum=number1+number2;
+console.log(sum);
+
+var dosomik=0.23658998855;
+dsomik=dosomik.toFixed(3);
+console.log(dsomik)
+
+
+
+// Conditional operator
+
+// var dam =1000;
+// if(dam>900){
+//     console.log("I will eat Biriani")
+// }
+// else{
+//   console.log= (" Sorry! baby vat is real")
+// }
+
+var biyasadi=true;
+var ammount=50000;
+if(biyasadi==true && ammount<50000){
+    console.log("Aso Biya kori");}
+else{
+    console.log("Tor kopale biya nai");
+}
