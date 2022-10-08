65e0cb31cde1bc1bc541abc52532d1b418439d40
Utso Sarkar <87900549+Utso-Sarkar@users.noreply.github.com>
2022-10-08 14:40:28 +0600

    fact done

---
+++ apply.js
@@ -0,0 +1,7 @@
+function ageCalculation(old){
+    var age=old/12;
+    return age;
+
+}
+var naniAge=ageCalculation(288);
+console.log(naniAge);
---
+++ factorial.js
@@ -0,0 +1,14 @@
+var facttorial=1
+for(var i=1;i<=10;i++){
+    facttorial=facttorial*i
+    console.log(i,facttorial)
+}
+
+// whileloops
+var i =1
+var facttorial=1
+while(i<=10){
+    facttorial=facttorial*i
+    console.log(i,facttorial)
+    i++
+}
